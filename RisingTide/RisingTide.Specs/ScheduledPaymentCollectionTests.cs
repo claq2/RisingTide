3b2c8c534d28898d6d13019a812391ba05f55e21
claq2 <claq2@yahoo.com>
2012-05-08 23:10:35 -0400

    More tests

--- RisingTide/RisingTide.Specs/ScheduledPaymentCollectionTests.cs
+++ RisingTide/RisingTide.Specs/ScheduledPaymentCollectionTests.cs
@@ -11,7 +11,7 @@ namespace RisingTide.Specs
 {
     public class ScheduledPaymentCollectionTests
     {
-        public class when_generating_a_list_of_90_days_with_1_weekly_1_dollar_payment
+        public class when_generating_a_list_of_90_days_with_1_weekly_1_dollar_payment_and_0_initial_balance
         {
             static Collection<ScheduledPayment> scheduledPayments;
             static List<CalendarDay> result;
@@ -23,7 +23,114 @@ namespace RisingTide.Specs
 
             Because of = () => result = scheduledPayments.GetDayRangeWithPaymentsFor(DateTime.Today, 90, 0);
 
+            It should_have_a_balance_of_minus_1_dollar_on_the_first_day = () => result[0].EndOfDayBalance.ShouldEqual(-1.00M);
+            It should_have_a_balance_of_minus_1_dollar_on_the_second_day = () => result[1].EndOfDayBalance.ShouldEqual(-1.00M);
+            It should_have_a_balance_of_minus_1_dollar_on_the_seventh_day = () => result[6].EndOfDayBalance.ShouldEqual(-1.00M);
+            It should_have_a_balance_of_minus_2_dollars_on_the_eighth_day = () => result[7].EndOfDayBalance.ShouldEqual(-2.00M);
             It should_have_a_balance_of_minus_12_dollars_on_the_last_day = () => result[89].EndOfDayBalance.ShouldEqual(-13.00M);
+        }
+
+        public class when_generating_a_list_of_90_days_with_1_biweekly_1_dollar_payment_and_0_initial_balance
+        {
+            static Collection<ScheduledPayment> scheduledPayments;
+            static List<CalendarDay> result;
+            Establish context = () =>
+            {
+                scheduledPayments = new Collection<ScheduledPayment>();
+                scheduledPayments.Add(new ScheduledPayment() { PaymentType = new PaymentType() { Name = PaymentType.Debit }, Recurrence = new Recurrence() { Name = Recurrence.Biweekly }, Amount = 1.00M });
+            };
+
+            Because of = () => result = scheduledPayments.GetDayRangeWithPaymentsFor(DateTime.Today, 90, 0);
+
+            It should_have_a_balance_of_minus_1_dollar_on_the_first_day = () => result[0].EndOfDayBalance.ShouldEqual(-1.00M);
+            It should_have_a_balance_of_minus_1_dollar_on_the_second_day = () => result[1].EndOfDayBalance.ShouldEqual(-1.00M);
+            It should_have_a_balance_of_minus_1_dollar_on_the_forteenth_day = () => result[13].EndOfDayBalance.ShouldEqual(-1.00M);
+            It should_have_a_balance_of_minus_2_dollars_on_the_fifteenth_day = () => result[15].EndOfDayBalance.ShouldEqual(-2.00M);
+            It should_have_a_balance_of_minus_7_dollars_on_the_last_day = () => result[89].EndOfDayBalance.ShouldEqual(-7.00M);
+        }
+
+        public class when_generating_a_list_of_90_days_with_1_monthly_1_dollar_payment_and_0_initial_balance
+        {
+            static Collection<ScheduledPayment> scheduledPayments;
+            static List<CalendarDay> result;
+            Establish context = () =>
+            {
+                scheduledPayments = new Collection<ScheduledPayment>();
+                scheduledPayments.Add(new ScheduledPayment() { PaymentType = new PaymentType() { Name = PaymentType.Debit }, Recurrence = new Recurrence() { Name = Recurrence.Monthly }, Amount = 1.00M });
+            };
+
+            Because of = () => result = scheduledPayments.GetDayRangeWithPaymentsFor(DateTime.Today, 90, 0);
+
+            It should_have_a_balance_of_minus_1_dollar_on_the_first_day = () => result[0].EndOfDayBalance.ShouldEqual(-1.00M);
+            It should_have_a_balance_of_minus_1_dollar_on_the_second_day = () => result[1].EndOfDayBalance.ShouldEqual(-1.00M);
+            It should_have_a_balance_of_minus_1_dollar_on_the_forteenth_day = () => result[13].EndOfDayBalance.ShouldEqual(-1.00M);
+            It should_have_a_balance_of_minus_2_dollars_a_month_from_today = () =>
+            {
+                int index = (DateTime.Today.AddMonths(1) - DateTime.Today).Days;
+                result[index].EndOfDayBalance.ShouldEqual(-2.00M);
+            };
+            It should_have_a_balance_of_minus_3_dollars_on_the_last_day = () => result[89].EndOfDayBalance.ShouldEqual(-3.00M);
+        }
+
+        public class when_generating_a_list_of_90_days_with_1_bimonthly_1_dollar_payment_and_0_initial_balance
+        {
+            static Collection<ScheduledPayment> scheduledPayments;
+            static List<CalendarDay> result;
+            Establish context = () =>
+            {
+                scheduledPayments = new Collection<ScheduledPayment>();
+                scheduledPayments.Add(new ScheduledPayment() { PaymentType = new PaymentType() { Name = PaymentType.Debit }, Recurrence = new Recurrence() { Name = Recurrence.Bimonthly }, Amount = 1.00M });
+            };
+
+            Because of = () => result = scheduledPayments.GetDayRangeWithPaymentsFor(DateTime.Today, 90, 0);
+
+            It should_have_a_balance_of_minus_1_dollar_on_the_first_day = () => result[0].EndOfDayBalance.ShouldEqual(-1.00M);
+            It should_have_a_balance_of_minus_1_dollar_on_the_second_day = () => result[1].EndOfDayBalance.ShouldEqual(-1.00M);
+            It should_have_a_balance_of_minus_1_dollar_on_the_forteenth_day = () => result[13].EndOfDayBalance.ShouldEqual(-1.00M);
+            It should_have_a_balance_of_minus_2_dollars_a_month_from_today = () =>
+            {
+                int index = (DateTime.Today.AddMonths(2) - DateTime.Today).Days;
+                result[index].EndOfDayBalance.ShouldEqual(-2.00M);
+            };
+            It should_have_a_balance_of_minus_3_dollars_on_the_last_day = () => result[89].EndOfDayBalance.ShouldEqual(-2.00M);
+        }
+
+        public class when_generating_a_list_of_90_days_with_1_weekly_1_dollar_payment_and_20_initial_balance
+        {
+            static Collection<ScheduledPayment> scheduledPayments;
+            static List<CalendarDay> result;
+            Establish context = () =>
+            {
+                scheduledPayments = new Collection<ScheduledPayment>();
+                scheduledPayments.Add(new ScheduledPayment() { PaymentType = new PaymentType() { Name = PaymentType.Debit }, Recurrence = new Recurrence() { Name = Recurrence.Weekly }, Amount = 1.00M });
+            };
+
+            Because of = () => result = scheduledPayments.GetDayRangeWithPaymentsFor(DateTime.Today, 90, 20);
+
+            It should_have_a_balance_of_19_dollars_on_the_first_day = () => result[0].EndOfDayBalance.ShouldEqual(19.00M);
+            It should_have_a_balance_of_19_dollars_on_the_second_day = () => result[1].EndOfDayBalance.ShouldEqual(19.00M);
+            It should_have_a_balance_of_19_dollars_on_the_seventh_day = () => result[6].EndOfDayBalance.ShouldEqual(19.00M);
+            It should_have_a_balance_of_18_dollars_on_the_eighth_day = () => result[7].EndOfDayBalance.ShouldEqual(18.00M);
+            It should_have_a_balance_of_7_dollars_on_the_last_day = () => result[89].EndOfDayBalance.ShouldEqual(7.00M);
+        }
+
+        public class when_generating_a_list_of_90_days_with_1_biweekly_1_dollar_payment_and_20_initial_balance
+        {
+            static Collection<ScheduledPayment> scheduledPayments;
+            static List<CalendarDay> result;
+            Establish context = () =>
+            {
+                scheduledPayments = new Collection<ScheduledPayment>();
+                scheduledPayments.Add(new ScheduledPayment() { PaymentType = new PaymentType() { Name = PaymentType.Debit }, Recurrence = new Recurrence() { Name = Recurrence.Biweekly }, Amount = 1.00M });
+            };
+
+            Because of = () => result = scheduledPayments.GetDayRangeWithPaymentsFor(DateTime.Today, 90, 20);
+
+            It should_have_a_balance_of_19_dollars_on_the_first_day = () => result[0].EndOfDayBalance.ShouldEqual(19.00M);
+            It should_have_a_balance_of_19_dollars_on_the_second_day = () => result[1].EndOfDayBalance.ShouldEqual(19.00M);
+            It should_have_a_balance_of_19_dollars_on_the_forteenth_day = () => result[13].EndOfDayBalance.ShouldEqual(19.00M);
+            It should_have_a_balance_of_18_dollars_on_the_fifteenth_day = () => result[15].EndOfDayBalance.ShouldEqual(18.00M);
+            It should_have_a_balance_of_13_dollars_on_the_last_day = () => result[89].EndOfDayBalance.ShouldEqual(13.00M);
         }
     }
 }
--- RisingTide/RisingTide.Specs/UserTests.cs
+++ RisingTide/RisingTide.Specs/UserTests.cs
@@ -17,30 +17,57 @@ namespace RisingTide.Specs
         {
             static User user;
             static ScheduledPayment scheduledPayment;
-            static IDomainContext domainContext = null;
+            static IDomainContext domainContext;
             Establish context = () =>
+            {
+                domainContext = new InMemoryDomainContext();
+                user = new User(domainContext) { Payments = new Collection<ScheduledPayment>() };
+                domainContext.Save<User>(user);
+                scheduledPayment = new ScheduledPayment()
                 {
-                    domainContext = new InMemoryDomainContext();
-                    user = new User(domainContext) { Payments = new Collection<ScheduledPayment>() };
-                    domainContext.Save<User>(user);
-                    scheduledPayment = new ScheduledPayment()
-                    {
-                        Amount = 25.00M,
-                        Payee = "Rogers",
-                        PaymentType = new PaymentType() { Name = PaymentType.Debit },
-                        Recurrence = new Recurrence() { Name = Recurrence.Monthly }
-                    };
-
-
+                    Amount = 25.00M,
+                    Payee = "Rogers",
+                    PaymentType = new PaymentType() { Name = PaymentType.Debit },
+                    Recurrence = new Recurrence() { Name = Recurrence.Monthly }
                 };
+            };
             Because of = () =>
-                {
-                    user.AddScheduledPayment(scheduledPayment);
-                };
+            {
+                user.AddScheduledPayment(scheduledPayment);
+            };
             It should_have_attached_the_scheduled_payment_to_the_user = () =>
+            {
+                user.Payments.Contains(scheduledPayment).ShouldBeTrue();
+            };
+        }
+
+        public class when_deleting_a_scheduled_payment_for_a_user
+        {
+            static User user;
+            static ScheduledPayment scheduledPayment;
+            static IDomainContext domainContext;
+            Establish context = () =>
+            {
+                domainContext = new InMemoryDomainContext();
+                user = new User(domainContext) { Payments = new Collection<ScheduledPayment>() };
+                domainContext.Save<User>(user);
+                scheduledPayment = new ScheduledPayment()
                 {
-                    user.Payments.Contains(scheduledPayment);
+                    Amount = 25.00M,
+                    Payee = "Rogers",
+                    PaymentType = new PaymentType() { Name = PaymentType.Debit },
+                    Recurrence = new Recurrence() { Name = Recurrence.Monthly }
                 };
+                user.AddScheduledPayment(scheduledPayment);
+            };
+            Because of = () =>
+            {
+                user.DeleteScheduledPayment(scheduledPayment);
+            };
+            It should_have_removed_the_scheduled_payment_from_the_user = () =>
+            {
+                user.Payments.Contains(scheduledPayment).ShouldBeFalse();
+            };
         }
     }
 }
